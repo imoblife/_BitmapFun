28c35b2811376e0d6d9df19cf74898ab3e2ff73c
thelou1s <thelou1s@yahoo.com>
2014-04-01 16:39:43 +0800

    去除加载后的背景图片 尝试替换processBitmap

--- bitmapfun/BitmapFun/src/main/src/com/example/android/bitmapfun/util/ImageFetcher.java
+++ bitmapfun/BitmapFun/src/main/src/com/example/android/bitmapfun/util/ImageFetcher.java
@@ -17,7 +17,13 @@
 package com.example.android.bitmapfun.util;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
+import android.graphics.Bitmap.Config;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
@@ -41,271 +47,338 @@ import java.net.URL;
  * A simple subclass of {@link ImageResizer} that fetches and resizes images fetched from a URL.
  */
 public class ImageFetcher extends ImageResizer {
-    private static final String TAG = "ImageFetcher";
-    private static final int HTTP_CACHE_SIZE = 10 * 1024 * 1024; // 10MB
-    private static final String HTTP_CACHE_DIR = "http";
-    private static final int IO_BUFFER_SIZE = 8 * 1024;
+	private static final String TAG = "ImageFetcher";
+	private static final int HTTP_CACHE_SIZE = 10 * 1024 * 1024; // 10MB
+	private static final String HTTP_CACHE_DIR = "http";
+	private static final int IO_BUFFER_SIZE = 8 * 1024;
 
-    private DiskLruCache mHttpDiskCache;
-    private File mHttpCacheDir;
-    private boolean mHttpDiskCacheStarting = true;
-    private final Object mHttpDiskCacheLock = new Object();
-    private static final int DISK_CACHE_INDEX = 0;
+	private DiskLruCache mHttpDiskCache;
+	private File mHttpCacheDir;
+	private boolean mHttpDiskCacheStarting = true;
+	private final Object mHttpDiskCacheLock = new Object();
+	private static final int DISK_CACHE_INDEX = 0;
 
-    /**
-     * Initialize providing a target image width and height for the processing images.
-     *
-     * @param context
-     * @param imageWidth
-     * @param imageHeight
-     */
-    public ImageFetcher(Context context, int imageWidth, int imageHeight) {
-        super(context, imageWidth, imageHeight);
-        init(context);
-    }
+	private Context mContext;
 
-    /**
-     * Initialize providing a single target image size (used for both width and height);
-     *
-     * @param context
-     * @param imageSize
-     */
-    public ImageFetcher(Context context, int imageSize) {
-        super(context, imageSize);
-        init(context);
-    }
+	/**
+	 * Initialize providing a target image width and height for the processing images.
+	 *
+	 * @param context
+	 * @param imageWidth
+	 * @param imageHeight
+	 */
+	public ImageFetcher(Context context, int imageWidth, int imageHeight) {
+		super(context, imageWidth, imageHeight);
+		init(context);
+	}
 
-    private void init(Context context) {
-        checkConnection(context);
-        mHttpCacheDir = ImageCache.getDiskCacheDir(context, HTTP_CACHE_DIR);
-    }
+	/**
+	 * Initialize providing a single target image size (used for both width and height);
+	 *
+	 * @param context
+	 * @param imageSize
+	 */
+	public ImageFetcher(Context context, int imageSize) {
+		super(context, imageSize);
+		init(context);
+	}
 
-    @Override
-    protected void initDiskCacheInternal() {
-        super.initDiskCacheInternal();
-        initHttpDiskCache();
-    }
+	private void init(Context context) {
+		mContext = context;
+		checkConnection(context);
+		mHttpCacheDir = ImageCache.getDiskCacheDir(context, HTTP_CACHE_DIR);
+	}
 
-    private void initHttpDiskCache() {
-        if (!mHttpCacheDir.exists()) {
-            mHttpCacheDir.mkdirs();
-        }
-        synchronized (mHttpDiskCacheLock) {
-            if (ImageCache.getUsableSpace(mHttpCacheDir) > HTTP_CACHE_SIZE) {
-                try {
-                    mHttpDiskCache = DiskLruCache.open(mHttpCacheDir, 1, 1, HTTP_CACHE_SIZE);
-                    if (BuildConfig.DEBUG) {
-                        Log.d(TAG, "HTTP cache initialized");
-                    }
-                } catch (IOException e) {
-                    mHttpDiskCache = null;
-                }
-            }
-            mHttpDiskCacheStarting = false;
-            mHttpDiskCacheLock.notifyAll();
-        }
-    }
+	@Override
+	protected void initDiskCacheInternal() {
+		super.initDiskCacheInternal();
+		initHttpDiskCache();
+	}
 
-    @Override
-    protected void clearCacheInternal() {
-        super.clearCacheInternal();
-        synchronized (mHttpDiskCacheLock) {
-            if (mHttpDiskCache != null && !mHttpDiskCache.isClosed()) {
-                try {
-                    mHttpDiskCache.delete();
-                    if (BuildConfig.DEBUG) {
-                        Log.d(TAG, "HTTP cache cleared");
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "clearCacheInternal - " + e);
-                }
-                mHttpDiskCache = null;
-                mHttpDiskCacheStarting = true;
-                initHttpDiskCache();
-            }
-        }
-    }
+	private void initHttpDiskCache() {
+		if (!mHttpCacheDir.exists()) {
+			mHttpCacheDir.mkdirs();
+		}
+		synchronized (mHttpDiskCacheLock) {
+			if (ImageCache.getUsableSpace(mHttpCacheDir) > HTTP_CACHE_SIZE) {
+				try {
+					mHttpDiskCache = DiskLruCache.open(mHttpCacheDir, 1, 1,
+							HTTP_CACHE_SIZE);
+					if (BuildConfig.DEBUG) {
+						Log.d(TAG, "HTTP cache initialized");
+					}
+				} catch (IOException e) {
+					mHttpDiskCache = null;
+				}
+			}
+			mHttpDiskCacheStarting = false;
+			mHttpDiskCacheLock.notifyAll();
+		}
+	}
 
-    @Override
-    protected void flushCacheInternal() {
-        super.flushCacheInternal();
-        synchronized (mHttpDiskCacheLock) {
-            if (mHttpDiskCache != null) {
-                try {
-                    mHttpDiskCache.flush();
-                    if (BuildConfig.DEBUG) {
-                        Log.d(TAG, "HTTP cache flushed");
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "flush - " + e);
-                }
-            }
-        }
-    }
+	@Override
+	protected void clearCacheInternal() {
+		super.clearCacheInternal();
+		synchronized (mHttpDiskCacheLock) {
+			if (mHttpDiskCache != null && !mHttpDiskCache.isClosed()) {
+				try {
+					mHttpDiskCache.delete();
+					if (BuildConfig.DEBUG) {
+						Log.d(TAG, "HTTP cache cleared");
+					}
+				} catch (IOException e) {
+					Log.e(TAG, "clearCacheInternal - " + e);
+				}
+				mHttpDiskCache = null;
+				mHttpDiskCacheStarting = true;
+				initHttpDiskCache();
+			}
+		}
+	}
 
-    @Override
-    protected void closeCacheInternal() {
-        super.closeCacheInternal();
-        synchronized (mHttpDiskCacheLock) {
-            if (mHttpDiskCache != null) {
-                try {
-                    if (!mHttpDiskCache.isClosed()) {
-                        mHttpDiskCache.close();
-                        mHttpDiskCache = null;
-                        if (BuildConfig.DEBUG) {
-                            Log.d(TAG, "HTTP cache closed");
-                        }
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "closeCacheInternal - " + e);
-                }
-            }
-        }
-    }
+	@Override
+	protected void flushCacheInternal() {
+		super.flushCacheInternal();
+		synchronized (mHttpDiskCacheLock) {
+			if (mHttpDiskCache != null) {
+				try {
+					mHttpDiskCache.flush();
+					if (BuildConfig.DEBUG) {
+						Log.d(TAG, "HTTP cache flushed");
+					}
+				} catch (IOException e) {
+					Log.e(TAG, "flush - " + e);
+				}
+			}
+		}
+	}
 
-    /**
-    * Simple network connection check.
-    *
-    * @param context
-    */
-    private void checkConnection(Context context) {
-        final ConnectivityManager cm =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        final NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        if (networkInfo == null || !networkInfo.isConnectedOrConnecting()) {
-            Toast.makeText(context, R.string.no_network_connection_toast, Toast.LENGTH_LONG).show();
-            Log.e(TAG, "checkConnection - no connection found");
-        }
-    }
+	@Override
+	protected void closeCacheInternal() {
+		super.closeCacheInternal();
+		synchronized (mHttpDiskCacheLock) {
+			if (mHttpDiskCache != null) {
+				try {
+					if (!mHttpDiskCache.isClosed()) {
+						mHttpDiskCache.close();
+						mHttpDiskCache = null;
+						if (BuildConfig.DEBUG) {
+							Log.d(TAG, "HTTP cache closed");
+						}
+					}
+				} catch (IOException e) {
+					Log.e(TAG, "closeCacheInternal - " + e);
+				}
+			}
+		}
+	}
 
-    /**
-     * The main process method, which will be called by the ImageWorker in the AsyncTask background
-     * thread.
-     *
-     * @param data The data to load the bitmap, in this case, a regular http URL
-     * @return The downloaded and resized bitmap
-     */
-    private Bitmap processBitmap(String data) {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "processBitmap - " + data);
-        }
+	/**
+	* Simple network connection check.
+	*
+	* @param context
+	*/
+	private void checkConnection(Context context) {
+		final ConnectivityManager cm = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		final NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+		if (networkInfo == null || !networkInfo.isConnectedOrConnecting()) {
+			Toast.makeText(context, R.string.no_network_connection_toast,
+					Toast.LENGTH_LONG).show();
+			Log.e(TAG, "checkConnection - no connection found");
+		}
+	}
 
-        final String key = ImageCache.hashKeyForDisk(data);
-        FileDescriptor fileDescriptor = null;
-        FileInputStream fileInputStream = null;
-        DiskLruCache.Snapshot snapshot;
-        synchronized (mHttpDiskCacheLock) {
-            // Wait for disk cache to initialize
-            while (mHttpDiskCacheStarting) {
-                try {
-                    mHttpDiskCacheLock.wait();
-                } catch (InterruptedException e) {}
-            }
+	/**
+	 * The main process method, which will be called by the ImageWorker in the AsyncTask background
+	 * thread.
+	 *
+	 * @param data The data to load the bitmap, in this case, a regular http URL
+	 * @return The downloaded and resized bitmap
+	 */
+	private Bitmap processBitmap(String data) {
+		if (BuildConfig.DEBUG) {
+			Log.d(TAG, "processBitmap - " + data);
+		}
 
-            if (mHttpDiskCache != null) {
-                try {
-                    snapshot = mHttpDiskCache.get(key);
-                    if (snapshot == null) {
-                        if (BuildConfig.DEBUG) {
-                            Log.d(TAG, "processBitmap, not found in http cache, downloading...");
-                        }
-                        DiskLruCache.Editor editor = mHttpDiskCache.edit(key);
-                        if (editor != null) {
-                            if (downloadUrlToStream(data,
-                                    editor.newOutputStream(DISK_CACHE_INDEX))) {
-                                editor.commit();
-                            } else {
-                                editor.abort();
-                            }
-                        }
-                        snapshot = mHttpDiskCache.get(key);
-                    }
-                    if (snapshot != null) {
-                        fileInputStream =
-                                (FileInputStream) snapshot.getInputStream(DISK_CACHE_INDEX);
-                        fileDescriptor = fileInputStream.getFD();
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "processBitmap - " + e);
-                } catch (IllegalStateException e) {
-                    Log.e(TAG, "processBitmap - " + e);
-                } finally {
-                    if (fileDescriptor == null && fileInputStream != null) {
-                        try {
-                            fileInputStream.close();
-                        } catch (IOException e) {}
-                    }
-                }
-            }
-        }
+		final String key = ImageCache.hashKeyForDisk(data);
+		FileDescriptor fileDescriptor = null;
+		FileInputStream fileInputStream = null;
+		DiskLruCache.Snapshot snapshot;
+		synchronized (mHttpDiskCacheLock) {
+			// Wait for disk cache to initialize
+			while (mHttpDiskCacheStarting) {
+				try {
+					mHttpDiskCacheLock.wait();
+				} catch (InterruptedException e) {
+				}
+			}
 
-        Bitmap bitmap = null;
-        if (fileDescriptor != null) {
-            bitmap = decodeSampledBitmapFromDescriptor(fileDescriptor, mImageWidth,
-                    mImageHeight, getImageCache());
-        }
-        if (fileInputStream != null) {
-            try {
-                fileInputStream.close();
-            } catch (IOException e) {}
-        }
-        return bitmap;
-    }
+			if (mHttpDiskCache != null) {
+				try {
+					snapshot = mHttpDiskCache.get(key);
+					if (snapshot == null) {
+						if (BuildConfig.DEBUG) {
+							Log.d(TAG,
+									"processBitmap, not found in http cache, downloading...");
+						}
+						DiskLruCache.Editor editor = mHttpDiskCache.edit(key);
+						if (editor != null) {
+							//							if (loadIconToStream(data,
+							//									editor.newOutputStream(DISK_CACHE_INDEX))) {
+							if (downloadUrlToStream(data,
+									editor.newOutputStream(DISK_CACHE_INDEX))) {
+								editor.commit();
+							} else {
+								editor.abort();
+							}
+						}
+						snapshot = mHttpDiskCache.get(key);
+					}
+					if (snapshot != null) {
+						fileInputStream = (FileInputStream) snapshot
+								.getInputStream(DISK_CACHE_INDEX);
+						fileDescriptor = fileInputStream.getFD();
+					}
+				} catch (IOException e) {
+					Log.e(TAG, "processBitmap - " + e);
+				} catch (IllegalStateException e) {
+					Log.e(TAG, "processBitmap - " + e);
+				} finally {
+					if (fileDescriptor == null && fileInputStream != null) {
+						try {
+							fileInputStream.close();
+						} catch (IOException e) {
+						}
+					}
+				}
+			}
+		}
 
-    @Override
-    protected Bitmap processBitmap(Object data) {
-        return processBitmap(String.valueOf(data));
-    }
+		Bitmap bitmap = null;
+		if (fileDescriptor != null) {
+			bitmap = decodeSampledBitmapFromDescriptor(fileDescriptor,
+					mImageWidth, mImageHeight, getImageCache());
+		}
+		if (fileInputStream != null) {
+			try {
+				fileInputStream.close();
+			} catch (IOException e) {
+			}
+		}
+		return bitmap;
+	}
 
-    /**
-     * Download a bitmap from a URL and write the content to an output stream.
-     *
-     * @param urlString The URL to fetch
-     * @return true if successful, false otherwise
-     */
-    public boolean downloadUrlToStream(String urlString, OutputStream outputStream) {
-        disableConnectionReuseIfNecessary();
-        HttpURLConnection urlConnection = null;
-        BufferedOutputStream out = null;
-        BufferedInputStream in = null;
+	@Override
+	protected Bitmap processBitmap(Object data) {
+		return processBitmap(String.valueOf(data));
+		//		return processBitmapForPackage(String.valueOf(data));
+	}
 
-        try {
-            final URL url = new URL(urlString);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            in = new BufferedInputStream(urlConnection.getInputStream(), IO_BUFFER_SIZE);
-            out = new BufferedOutputStream(outputStream, IO_BUFFER_SIZE);
+	/**
+	 * Download a bitmap from a URL and write the content to an output stream.
+	 *
+	 * @param urlString The URL to fetch
+	 * @return true if successful, false otherwise
+	 */
+	public boolean downloadUrlToStream(String urlString,
+			OutputStream outputStream) {
+		disableConnectionReuseIfNecessary();
+		HttpURLConnection urlConnection = null;
+		BufferedOutputStream out = null;
+		BufferedInputStream in = null;
 
-            int b;
-            while ((b = in.read()) != -1) {
-                out.write(b);
-            }
-            return true;
-        } catch (final IOException e) {
-            Log.e(TAG, "Error in downloadBitmap - " + e);
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            try {
-                if (out != null) {
-                    out.close();
-                }
-                if (in != null) {
-                    in.close();
-                }
-            } catch (final IOException e) {}
-        }
-        return false;
-    }
+		try {
+			final URL url = new URL(urlString);
+			urlConnection = (HttpURLConnection) url.openConnection();
+			in = new BufferedInputStream(urlConnection.getInputStream(),
+					IO_BUFFER_SIZE);
+			out = new BufferedOutputStream(outputStream, IO_BUFFER_SIZE);
 
-    /**
-     * Workaround for bug pre-Froyo, see here for more info:
-     * http://android-developers.blogspot.com/2011/09/androids-http-clients.html
-     */
-    public static void disableConnectionReuseIfNecessary() {
-        // HTTP connection reuse which was buggy pre-froyo
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO) {
-            System.setProperty("http.keepAlive", "false");
-        }
-    }
+			int b;
+			while ((b = in.read()) != -1) {
+				out.write(b);
+			}
+			return true;
+		} catch (final IOException e) {
+			Log.e(TAG, "Error in downloadBitmap - " + e);
+		} finally {
+			if (urlConnection != null) {
+				urlConnection.disconnect();
+			}
+			try {
+				if (out != null) {
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (final IOException e) {
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Workaround for bug pre-Froyo, see here for more info:
+	 * http://android-developers.blogspot.com/2011/09/androids-http-clients.html
+	 */
+	public static void disableConnectionReuseIfNecessary() {
+		// HTTP connection reuse which was buggy pre-froyo
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO) {
+			System.setProperty("http.keepAlive", "false");
+		}
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////
+
+	private Bitmap processBitmapForPackage(String pkgName) {
+		Log.i(TAG, "processBitmapForPackage(): " + pkgName);
+		Bitmap bitmap = null;
+		try {
+			PackageManager pm = mContext.getPackageManager();
+			ApplicationInfo ai = pm.getApplicationInfo(pkgName, 1);
+			Drawable d = ai.loadIcon(pm);
+			bitmap = drawableToBitmap(mContext, d);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return bitmap;
+	}
+
+	public Bitmap drawableToBitmap(Context context, Drawable drawable) {
+		if (drawable == null) {
+			return null;
+		}
+
+		float d = context.getResources().getDisplayMetrics().density;
+		int w = (int) (drawable.getIntrinsicWidth() * d);
+		int h = (int) (drawable.getIntrinsicHeight() * d);
+		Config cf = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+				: Bitmap.Config.RGB_565;
+		Bitmap bitmap = Bitmap.createBitmap(w, h, cf);
+		Canvas c = new Canvas(bitmap);
+		drawable.setBounds(0, 0, w, h);
+		drawable.draw(c);
+		return bitmap;
+	}
+
+	private boolean loadIconToStream(String pkgName, OutputStream outputStream) {
+		BufferedOutputStream out = null;
+		try {
+			Bitmap bitmap = processBitmapForPackage(pkgName);
+			out = new BufferedOutputStream(outputStream, IO_BUFFER_SIZE);
+			bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return true;
+	}
 }
